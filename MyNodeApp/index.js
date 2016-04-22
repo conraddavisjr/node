65e5233e325acf696b9656492c24ddf3d805cc49
Conrad Davis Jr <conrad.davis@akqa.com>
2016-04-21 18:16:25 -0700

    myNodeApp

---
+++ MyNodeApp/index.js
@@ -0,0 +1,26 @@
+// var http = require('http');
+
+// var server = http.createServer(function (request, response){
+// 	response.writeHead(200, {"content-Type": "text/plain"});
+// 	response.end("Hello world");
+// })
+
+
+// server.listen(7000);
+
+// console.log("web page up at localhost:7000");
+
+// var fs = require("fs");
+
+// fs.readFile('hello.txt', function(err, buf){
+// 	console.log(buf.toString());
+// })
+
+// console.log("this happened when..?")
+
+
+// var fs = require("fs");
+// var content = fs.readFileSync('hello.txt').toString();
+// console.log(content);
+// console.log("this happened when..?")
+
